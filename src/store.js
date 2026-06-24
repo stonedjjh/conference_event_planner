31f117df07351479467edecc0a462a52a3d7525b
stonedjjh <danjim82@gmail.com>
2026-06-24 08:54:57 -0400

    feat: se agregó el slice de comida a la tienda
    fix: se corrigió el llamado al slice de accesorios.

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-// se importa el slice de accesorios
+// se importa el slice de accesorios y comidas
 import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
+
 export default configureStore({
   reducer: {
     venue: venueReducer,
-    //se agrega el slice de accesorio a la tienda
-    av: avSlice,
+    //se agrega el slice de accesorio y comidas a la tienda
+    av: avReducer,// Se corrigió esta línea
+    meals:mealsReducer
   },
 });
