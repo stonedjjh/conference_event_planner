242eab63f5e28629ce85cd84bc504a718ac6268b
stonedjjh <danjim82@gmail.com>
2026-06-24 08:27:36 -0400

    feat: importación e inclusión del slice de accesorio en la store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+// se importa el slice de accesorios
+import avReducer from './avSlice';
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    //se agrega el slice de accesorio a la tienda
+    av: avSlice,
   },
 });
